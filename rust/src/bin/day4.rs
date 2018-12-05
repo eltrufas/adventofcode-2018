839c8aef871cef511202ab41abd8564c5c51cf36
Rafael Castillo <rafael@trfs.me>
2018-12-05 01:11:54 -0300

    Solution without chronos

--- rust/src/bin/day4.rs
+++ rust/src/bin/day4.rs
@@ -1,11 +1,9 @@
 extern crate regex;
-extern crate chrono;
 
 use std::io::prelude::*;
 use std::io;
 use regex::Regex;
 use std::collections::HashMap;
-use chrono::{NaiveDateTime, Timelike};
 
 enum GuardAction {
     BeginShift(i32),
@@ -13,33 +11,25 @@ enum GuardAction {
     WakeUp,
 }
 
-struct LogEntry(NaiveDateTime, GuardAction);
-
-/*
-[1518-06-27 00:50] wakes up
-[1518-09-01 00:03] Guard #1601 begins shift
-[1518-03-11 00:03] Guard #1601 begins shift
-[1518-10-02 00:24] falls asleep
-[1518-04-30 00:00] Guard #2887 begins shift
-[1518-11-15 23:57] Guard #1097 begins shift
-[1518-11-05 00:19] falls asleep
-[1518-07-21 00:03] Guard #1249 begins shift
-[1518-08-29 00:59] wakes up
-*/
-
 fn main() {
     let stdin = io::stdin();
-    let entry_re = Regex::new(r"^\[(.*)\](.*)$").unwrap();
+    let entry_re = Regex::new(r"^\[.*:(\d+)\](.*)$").unwrap();
     let shift_re = Regex::new(r"Guard #(\d+) begins shift").unwrap();
-    let mut log: Vec<_> = stdin.lock()
+    let mut lines: Vec<_> = stdin.lock()
         .lines()
         .flatten()
+        .collect();
+
+    lines.sort_unstable();
+
+    let (sleep_times, _, _)= lines
+        .iter()
         .map(|l| {
             entry_re.captures(&l).and_then(|captures| {
                 let time = captures.get(1)
                     .map(|m| m.as_str())
                     .and_then(|s| {
-                        NaiveDateTime::parse_from_str(s, "%Y-%m-%d %H:%M").ok()
+                        s.parse().ok()
                     });
 
                 let action = captures.get(2)
@@ -47,56 +37,30 @@ fn main() {
                         match m.as_str().trim() {
                             s if s == "wakes up" => Some(GuardAction::WakeUp),
                             s if s == "falls asleep" => Some(GuardAction::FallAsleep),
-                            s => {
-                                let s = shift_re.captures(s)
-                                    .and_then(|c| c.get(1))
-                                    .and_then(|m| m.as_str().parse().ok());
-                                Some(GuardAction::BeginShift(s.unwrap()))
-                            }
+                            s => shift_re.captures(s)
+                                .and_then(|c| c.get(1))
+                                .and_then(|m| m.as_str().parse().ok())
+                                .map(|s| GuardAction::BeginShift(s)),
                         }
                     });
 
-                action.and_then(|action| time.map(|time| LogEntry(time, action)))
+                action.and_then(|action| time.map(|time| (time, action)))
             })
         })
         .flatten()
-        .collect();
+        .fold((HashMap::new(), 0, 0), |(mut sleep_times, m, guard), (time, action)| {
+            match action {
+                GuardAction::WakeUp => {
+                    sleep_times.entry(guard)
+                        .or_insert_with(|| vec![0; 60])[m..time]
+                        .iter_mut().for_each(|t| *t += 1);
 
-    log.sort_unstable_by_key(|e| e.0);
-
-    let mut m: usize = 0;
-    let mut sleep_times: HashMap<i32, Vec<i32>> = HashMap::new();
-    let mut guard = 0;
-    for LogEntry(time, action) in log {
-        let new_m = time.minute() as usize;
-        match action {
-            GuardAction::WakeUp => {
-                sleep_times.entry(guard)
-                    .or_insert_with(|| vec![0; 60])[m..new_m]
-                    .iter_mut().for_each(|t| *t += 1);
-            },
-            GuardAction::BeginShift(g) => {
-                guard = g;
-                if guard == 2381 {
-                    println!("{} New guard {}", time, g);
-                }
-            },
-            _ => {
-                if guard == 2381 {
-                    println!("{} Guard sleeping", time);
-                }
+                    (sleep_times, time, guard)
+                },
+                GuardAction::BeginShift(g) => (sleep_times, time, g),
+                GuardAction::FallAsleep => (sleep_times, time, guard)
             }
-        };
-        m = new_m;
-    }
-
-    /*log.iter()
-        .filter(|LogEntry(_, a)| {
-            if GuardAction::
-        }).foreach(|LogEntry(t, a)| {
-            println()
-        })
-        */
+        });
 
     sleep_times.iter().max_by_key(|(_, t)| -> i32 { t.iter().sum() }).map(|(g, t)| {
         let (minute, _) = t.iter().enumerate().max_by_key(|(_, t)| t.clone()).unwrap();
@@ -112,11 +76,6 @@ fn main() {
         .max_by_key(|(_, _, t)| t.clone())
         .map(|(g, i, _t)| {
             let minute = i as i32;
-            println!("Part one: {} * {} = {}", g, minute, g * minute);
+            println!("Part two: {} * {} = {}", g, minute, g * minute);
         });
-
-    /*log.into_iter.fold((Vec::new(), 0, false), |(shifts, t, asleep), (time, action)| {
-        match action {
-        }
-    });*/
 }
